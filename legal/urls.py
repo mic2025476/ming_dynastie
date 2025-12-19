24d0957286ff9a2bf22a5db0157fdd2af1ddd631
anirudhflutter <anirudhflutter@gmail.com>
2025-12-19 12:34:57 +0100

    included impressum and Datenschutz

--- legal/urls.py
+++ legal/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name='legal'
-urlpatterns=[path('impressum/', views.impressum, name='impressum'), path('datenschutz/', views.datenschutz, name='datenschutz')] 
+app_name = "legal"
+
+urlpatterns = [
+    path("impressum/", views.impressum, name="impressum"),
+    path("datenschutz/", views.datenschutz, name="datenschutz"),
+]
--- legal/views.py
+++ legal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-def impressum(request): return render(request, 'legal/impressum.html')
-def datenschutz(request): return render(request, 'legal/datenschutz.html')
+
+def impressum(request):
+    return render(request, "legal/impressum.html")
+
+def datenschutz(request):
+    return render(request, "legal/datenschutz.html")
