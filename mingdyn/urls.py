ae6fec3305ea994cda94d404205254e2c97a1377
anirudhflutter <anirudhflutter@gmail.com>
2026-04-29 11:19:46 +0200

    tiny change

--- mingdyn/urls.py
+++ mingdyn/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.i18n import JavaScriptCatalog
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
@@ -15,6 +15,7 @@ urlpatterns = [
     path('ming/legal/', include('legal.urls')),
     path("", include("qrflow.urls")),
     path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
 ]
 
 if settings.DEBUG:
